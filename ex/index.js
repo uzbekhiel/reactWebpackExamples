089df5552fec6a135f406ff5199e83e744fe8160
Saulo dos Santos Firmino <saulofirmino@ymail.com>
2018-07-23 00:32:26 -0300

    fazendo exercícios do curso

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,8 @@
-console.log('Webpack')
+import  'react'
+import './estilo.css'
+
+export default props => (
+    <h1>Olá</h1>
+)
+
+console.log("Funcionou!!")
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,38 @@
 const webpack = require('webpack')
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 module.exports = {
+    mode:'development',
     entry: './ex/index.js',
     output:{
-        path: __dirname + './public',
+        path: __dirname + '/public',
         filename: './bundle.js'
     },
     devServer: {
         port: 2018,
         contentBase: './public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "app.css"
+        })
+    ],
+    module:{
+        rules:[{
+            test: /.js?$/,
+            loader : 'babel-loader',
+            exclude: '/node_modules/',
+            query: {
+                presets: ['es2015','react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        },{
+            test: /\.css$/,
+            use : [
+                    MiniCssExtractPlugin.loader,
+                    //'style-loader',
+                    'css-loader',
+
+            ]
+        }]
     }
 }
